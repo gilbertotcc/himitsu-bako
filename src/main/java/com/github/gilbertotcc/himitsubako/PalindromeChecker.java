42dd3e09f84b69460560b45d8b1b0c822de18791
Gilberto Taccari <gilberto.taccari@gmail.com>
2018-04-09 22:24:14 +0200

    Add 'PalindromeCheckerTest'

---
+++ src/main/java/com/github/gilbertotcc/himitsubako/PalindromeChecker.java
@@ -0,0 +1,26 @@
+package com.github.gilbertotcc.himitsubako;
+
+import java.util.List;
+
+public class PalindromeChecker {
+
+    private PalindromeChecker() {}
+
+
+
+    public static boolean isPalindromicNumber(final long aNumber) {
+
+        // TODO Do something ...
+
+        return false;
+    }
+
+
+
+    public static List<Long> palindromicNumbersIn(List<Long> aList) {
+
+        // TODO Do something ...
+
+        return aList;
+    }
+}
